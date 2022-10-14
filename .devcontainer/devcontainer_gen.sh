f5be6b1147bd51221d37ba19e09ed5d92aacfed6
Simon Meggle <simon.meggle@elabit.de>
2022-10-14 13:37:18 +0200

    Devcontainer name

--- .devcontainer/devcontainer_gen.sh
+++ .devcontainer/devcontainer_gen.sh
@@ -2,15 +2,29 @@
 # SPDX-FileCopyrightText: © 2022 ELABIT GmbH <mail@elabit.de>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+function main() {
+    PROJECT_DIR="$(dirname $(folder_of $0))"
+    PROJECT=${PROJECT_DIR##*/} 
+    export CONTAINER_NAME=${PROJECT}-devc
 
-echo "+ Generating CMK devcontainer file ..."
-# Ref LeP3qq
-envsubst < .devcontainer/devcontainer_tpl.json > .devcontainer/devcontainer.json
-# devcontainer.json contains a VS Code Variable ${containerWorkspaceFolder}, which would also 
-# be processed by envsubst. To avoid this, the template files contain ###{containerWorkspaceFolder}.
-# The three hashes are replaced with $ _after_ envsusbt has done its work. 
-# Mac-only sed... 
-sed -i "" 's/###/$/' .devcontainer/devcontainer.json
+    echo "+ Generating CMK devcontainer file ..."
+    # Ref LeP3qq
+    envsubst < .devcontainer/devcontainer_tpl.json > .devcontainer/devcontainer.json
+    # devcontainer.json contains a VS Code Variable ${containerWorkspaceFolder}, which would also 
+    # be processed by envsubst. To avoid this, the template files contain ###{containerWorkspaceFolder}.
+    # The three hashes are replaced with $ _after_ envsusbt has done its work. 
+    # Mac-only sed... 
+    sed -i "" 's/###/$/' .devcontainer/devcontainer.json
 
-echo ">>> Preparation for Checkmk version $VERSION finished."
-echo "You can now start the devcontainer in VS Code with 'Remote-Containers: Rebuild Container'."
+    echo ">>> Preparation for Checkmk version $VERSION finished."
+    echo "You can now start the devcontainer in VS Code with 'Remote-Containers: Rebuild Container'."
+}
+
+
+function folder_of() {
+  DIR="${1%/*}"
+  (cd "$DIR" && echo "$(pwd -P)")
+}
+
+
+main $@
